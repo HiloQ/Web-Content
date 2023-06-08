a9c54862c8b4e7cb9cbb5657aa4c8f64c9b68c0d
Jakub Sukiennik <lordquick@icloud.com>
2023-06-08 11:05:12 +0200

    Initial Commit

---
+++ Web Content/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Web Content
+//
+//  Created by Jakub Sukiennim on 08/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
